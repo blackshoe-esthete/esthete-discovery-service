50329b15cd3af0418a0806c355017ac1c500a917
Subin <70842920+bin-pro@users.noreply.github.com>
2024-03-09 15:21:14 +0900

    Build: Init discovery service

---
+++ src/main/java/com/blackshoe/esthete/EstheteDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blackshoe.esthete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstheteDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstheteDiscoveryServiceApplication.class, args);
+	}
+
+}
